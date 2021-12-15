9e2f2c34a617091c9535ced2970200b3d3d780fb
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-15 13:59:37 +0700

    Make atom core class

--- resources/js/core/components/atom.jsx
+++ resources/js/core/components/atom.jsx
@@ -1,5 +1,30 @@
 import Component from './index'
 
 export default class Atom extends Component {
-  
+  compileClasses() {
+    let classes = this.classes
+
+    this.classMethods.forEach(methodName => {
+      classes.push(...this[methodName]())
+    })
+
+    if (typeof this.props.className == 'string') {
+      classes = classes.join(' ') + ' ' + this.props.className
+    } else if (Array.isArray(this.props.className)) {
+      classes = classes.push(...this.props.className)
+      classes = classes.join(' ')
+    } else {
+      classes = classes.join(' ')
+    }
+
+    return classes
+  }
+
+  _getDefaultOfMappedClasses(classesMap, index) {
+    let classes = classesMap[index]
+    classes = classes == undefined ? classesMap['_default'] : classes
+    classes = classes == undefined ? '' : classes
+
+    return classes == '' ? [] : [classes]
+  }
 }
