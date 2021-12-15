939e4a7c7f94999de7d71afb9eed88f85f791975
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-15 15:00:00 +0700

    Changing the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  darkMode: false,
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.jsx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.purple,
+        secondary: colors.gray,
+        success: colors.green,
+        danger: colors.red,
+        warning: colors.yellow,
+        info: colors.teal,
+        dark: colors.zinc,
+        light: colors.slate,
+      }
+    },
   },
   plugins: [],
 }
