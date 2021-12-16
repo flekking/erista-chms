4584d8a3de8a0229d2deb5de34780ac234ffc3d6
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-16 16:18:22 +0700

    Fixing auth layout

--- resources/js/layouts/auth/index.jsx
+++ resources/js/layouts/auth/index.jsx
@@ -11,14 +11,18 @@ export default class Auth extends Layout {
             src="/img/static/erista-logo-landscape.png" alt="Erista Logo"
           />
           <img
-            className="hidden lg:absolute lg:my-auto lg:left-0 lg:block lg:w-7/12 z-10"
+            className="hidden lg:absolute lg:my-auto lg:left-0 lg:block lg:w-7/12"
             src="/img/static/people-brainstorm.svg" alt="Filler Image"
           />
           <div
             className="mx-auto bg-white dark:bg-gray-800 p-6 md:bg-transparent lg:grid lg:grid-cols-2 items-center justify-center md:mt-0 w-full lg:max-w-screen-xl lg:relative 2xl:max:max-w-screen-xl xl:p-0"
           >
             <div className="hidden lg:block lg:w-full w-2/3"></div>
-            {this.propChildren()}
+            <div
+              className="bg-white dark:bg-gray-900 shadow rounded-lg w-full md:max-w-md p-6 pb-12 sm:p-8 md:mx-auto lg:px-12 lg:pt-6 lg:pb-16 md:pt-4 md:pb-16 space-y-8 z-0"
+            >
+              {this.propChildren()}
+            </div>
           </div>
         </div>
       </Unauthenticated>
