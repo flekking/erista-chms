733fa07113ca7d886160194809ab6729e98a96b1
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-16 17:20:07 +0700

    Make login page

--- resources/js/pages/login/index.jsx
+++ resources/js/pages/login/index.jsx
@@ -1,12 +1,37 @@
 import BasePage from '@erista/core/page'
 import AuthLayout from '@erista/layouts/auth'
 
-export default class Login extends BasePage {
+import { Heading } from '@erista/components/atoms/typography'
+import { Link } from '@erista/components/atoms/link'
+import LoginForm from '@erista/forms/login'
+
+class Login extends BasePage {
   render() {
     return (
       <AuthLayout>
-        
+        <div className="text-right">
+          <Link href="#">Back</Link>
+        </div>
+        <Heading level="5">Log in to Erista</Heading>
+        <LoginForm
+          onLostPassword={ e => this.onLostPassword(e) }
+          onCreateAccount={ e => this.onCreateAccount(e) }
+        />
       </AuthLayout>
     )
   }
+
+  onLostPassword(e) {
+    this.props.navigation('/web/reset_password')
+  }
+
+  onCreateAccount(e) {
+
+  }
+}
+
+import { useNavigate } from 'react-router-dom'
+export default function (props) {
+  const navigation = useNavigate()
+  return <Login {...props} navigation={navigation}/>
 }
