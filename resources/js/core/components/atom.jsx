b4da650caf984f135bb42466a3d966118c34c381
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-16 09:48:15 +0700

    Changing core components name

--- resources/js/core/components/atom.jsx
+++ resources/js/core/components/atom.jsx
@@ -1,42 +1,5 @@
 import Component from './index'
 
 export default class Atom extends Component {
-  _getCompiledClasses() {
-    let classes = this.classes
-
-    this.classMethods.forEach(methodName => {
-      classes.push(...this[methodName]())
-    })
-
-    if (typeof this.props.className == 'string') {
-      classes = classes.join(' ') + ' ' + this.props.className
-    } else if (Array.isArray(this.props.className)) {
-      classes = classes.push(...this.props.className)
-      classes = classes.join(' ')
-    } else {
-      classes = classes.join(' ')
-    }
-
-    return classes
-  }
-
-  _getDefaultOfMap(map, index) {
-    let item = map[index]
-    item = item == undefined ? map['_default'] : item
-    item = item == undefined ? '' : item
-
-    return item
-  }
-
-  _getDefaultOfMappedClasses(classesMap, index) {
-    let classes = this._getDefaultOfMap(classesMap, index)
-
-    return classes == '' ? [] : [classes]
-  }
-
-  onClick(e) {
-    if (typeof this.props.onClick == 'function') {
-      this.props.onClick(e)
-    }
-  }
+  
 }
--- resources/js/core/components/index.jsx
+++ resources/js/core/components/index.jsx
@@ -1,5 +1,67 @@
 import React, { Component as BaseComponent } from 'react'
 
 export default class Component extends BaseComponent {
-  
+  /**
+   * Classes functionalities.
+   */
+  classes = []
+  classMethods = []
+
+  _getCompiledClasses() {
+    let classes = this.classes
+
+    this.classMethods.forEach(methodName => {
+      classes.push(...this[methodName]())
+    })
+
+    if (typeof this.props.className == 'string') {
+      classes = classes.join(' ') + ' ' + this.props.className
+    } else if (Array.isArray(this.props.className)) {
+      classes = classes.push(...this.props.className)
+      classes = classes.join(' ')
+    } else {
+      classes = classes.join(' ')
+    }
+
+    return classes
+  }
+
+  _getDefaultOfMap(map, index) {
+    let item = map[index]
+    item = item == undefined ? map['_default'] : item
+    item = item == undefined ? '' : item
+
+    return item
+  }
+
+  _getDefaultOfMappedClasses(classesMap, index) {
+    let classes = this._getDefaultOfMap(classesMap, index)
+
+    return classes == '' ? [] : [classes]
+  }
+
+  /**
+  * Props functionalities.
+  */
+  _defaultProps = []
+
+  propSize() {
+    return this.props.size || this._defaultProps.size || 'md'
+  }
+
+  propColor() {
+    return this.props.color || this._defaultProps.color || 'primary'
+  }
+
+  onClick(e) {
+    if (typeof this.props.onClick == 'function') {
+      this.props.onClick(e)
+    }
+  }
+
+  onSubmit(e) {
+    if (typeof this.props.onSubmit == 'function') {
+      this.props.onSubmit(e)
+    }
+  }
 }
