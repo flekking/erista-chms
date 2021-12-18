443777cd6d2c59cd37396720c5bb77f044c30d72
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-18 11:13:35 +0700

    Add header to dashboard

--- resources/js/pages/dashboard/index.jsx
+++ resources/js/pages/dashboard/index.jsx
@@ -1,11 +1,13 @@
 import Page from '@erista/core/page'
 import MainLayout from '@erista/layouts/main'
 
+import PageHeader from '@erista/components/molecules/pageHeader'
+
 export default class Dashboard extends Page {
   render() {
     return (
       <MainLayout>
-        <div>dashboard</div>
+        <PageHeader title="Dashboard" pages={[{ name: 'Dashboard', href: '#', current: true }]}/>
       </MainLayout>
     )
   }
