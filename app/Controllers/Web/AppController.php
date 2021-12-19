699f98754d1ff1991e4f62eaba1305b9a74e3c95
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-19 09:59:23 +0700

    Adding IndexRequest to AppController

--- app/Controllers/Web/AppController.php
+++ app/Controllers/Web/AppController.php
@@ -3,10 +3,11 @@
 namespace App\Controllers\Web;
 
 use Core\Controller;
+use App\Requests\Web\App\IndexRequest;
 
 class AppController extends Controller
 {
-    public function index()
+    public function index(IndexRequest $request)
     {
         return view('web.app');
     }
---
+++ app/Requests/Web/App/IndexRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Requests\Web\App;
+
+use Core\Http\FormRequest;
+
+class IndexRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [];
+    }
+}
