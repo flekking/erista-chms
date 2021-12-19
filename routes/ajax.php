fbd51d3d93d542b380318c76f6035121e748dda1
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-20 01:40:29 +0700

    Adding routes for member

--- routes/ajax.php
+++ routes/ajax.php
@@ -3,19 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 Route::domain('{churchSubdomain}.' . env('SITE_URL'))->group(function () {
+
+
     Route::prefix('utils')->namespace('Utils')->group(function () {
         Route::get('csrf_cookie', 'CsrfCookieController@show');
         Route::get('authenticated', 'AuthenticatedController@index');
     });
 
+
     Route::post('log/authentication_log/login', 'Log\\AuthenticationLogController@storeLogin');
+
+
+
 
     Route::middleware(['auth:web', 'auth.container'])->group(function () {
 
-        Route::prefix('log')->namespace('Log')->group(function () {
 
+        Route::prefix('log')->namespace('Log')->group(function () {
             Route::prefix('authentication_log')->group(function () {
                 Route::post('logout', 'AuthenticationLogController@storeLogout');
+            });
+        });
+
+
+        Route::prefix('people')->namespace('People')->group(function () {
+            Route::prefix('member')->group(function () {
+                Route::post('', 'MemberController@store');
             });
         });
     });
