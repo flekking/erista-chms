47bca29596031314de556f3d7525d1f0ce29160b
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-19 10:06:24 +0700

    Register ajax routing

--- routes/ajax.php
+++ routes/ajax.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('utils')->namespace('Utils')->group(function () {
     Route::get('csrf_cookie', 'CsrfCookieController@show');
 });
+
+Route::post('log/authentication_log/login', 'Log\\AuthenticationLogController@storeLogin');
