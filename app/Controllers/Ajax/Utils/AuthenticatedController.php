9f1c4bf01256e21fba704379c3c017f1b1d89ec1
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-19 20:31:53 +0700

    Add check based on church

--- app/Controllers/Ajax/Utils/AuthenticatedController.php
+++ app/Controllers/Ajax/Utils/AuthenticatedController.php
@@ -4,14 +4,14 @@ namespace App\Controllers\Ajax\Utils;
 
 use Core\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Core\Auth\Container;
 
 class AuthenticatedController extends Controller
 {
     public function index(Request $request)
     {
         return [
-            'authenticated' => Auth::check(),
+            'authenticated' => Container::check(),
         ];
     }
 }
