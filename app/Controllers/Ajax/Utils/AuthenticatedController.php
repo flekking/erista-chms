ad1253b0ecf9eb55db53e60b3b70866e32d2d564
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-19 19:48:55 +0700

    Add request to the AuthenticatedController

--- app/Controllers/Ajax/Utils/AuthenticatedController.php
+++ app/Controllers/Ajax/Utils/AuthenticatedController.php
@@ -3,14 +3,15 @@
 namespace App\Controllers\Ajax\Utils;
 
 use Core\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         return [
-            'authenticated' => Auth::check()
+            'authenticated' => Auth::check(),
         ];
     }
 }
