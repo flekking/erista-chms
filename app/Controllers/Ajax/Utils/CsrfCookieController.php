a4a6256bfb9be277e6bc4dd3ee689eaa49c58c7c
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-12-19 09:46:41 +0700

    Make csrf cookie controller

---
+++ app/Controllers/Ajax/Utils/CsrfCookieController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Ajax\Utils;
+
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController as Controller;
+
+class CsrfCookieController extends Controller
+{
+    //
+}
